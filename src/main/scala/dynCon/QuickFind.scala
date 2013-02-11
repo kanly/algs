1dec86228425a6ef925a8d69e388820dad42857c
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-02-11 18:39:48 +0100

    Delete debug messages

--- src/main/scala/dynCon/QuickFind.scala
+++ src/main/scala/dynCon/QuickFind.scala
@@ -2,18 +2,12 @@ package dynCon
 
 class QuickFind(elements: List[Int]) extends UF {
   def this(n: Int) = {
-    this(0 to n-1 toList)
+    this(0 to n - 1 toList)
   }
 
-  def union(p: Int, q: Int): UF = {
-    val ret = new QuickFind(elements.map(x => if (x == p) q else x))
-    println("p:"+p+", q:",q,". Resulting list: ",ret.toString)
-    ret
-  }
+  def union(p: Int, q: Int): UF = new QuickFind(elements.map(x => if (x == p) q else x))
 
-  def connected(p: Int, q: Int): Boolean = {
-    println(elements(p) + "==" + elements(q) )
-    elements(p) == elements(q)  }
+  def connected(p: Int, q: Int): Boolean = elements(p) == elements(q)
 
   override def toString = elements.mkString(";")
 }
