278fa81b66af3945251c48bfeffd1a53a85fd2a4
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-02-11 18:25:01 +0100

    Behaviour tests

---
+++ src/test/scala/dynCon/UFTest.scala
@@ -0,0 +1,30 @@
+package dynCon
+
+import org.scalatest.FlatSpec
+
+
+class UFTest extends FlatSpec with UFBehaviors{
+
+  def quickFind = new QuickFind(10)
+
+   "Quick find " should behave like _10SizedUF(quickFind)
+}
+
+trait UFBehaviors {
+  this: FlatSpec =>
+
+  def _10SizedUF(uf: => UF) {
+
+    it should "not find a connection" in {
+      val linkedUf = uf.union(0, 2).union(1, 3).union(0, 1).union(7, 8).union(8, 9).union(6, 7).union(3, 5)
+      assert(!linkedUf.connected(0, 9))
+    }
+
+    it should "find a connection" in {
+      val linkedUf = uf.union(0,3).union(3,8).union(8,2).union(2,5)
+      assert(linkedUf.connected(5,3))
+      assert(linkedUf.connected(3,5))
+    }
+  }
+}
+
