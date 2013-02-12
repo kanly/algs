2ef94e89752dcbaf1d1b72fc479b77ef4af49ba4
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-02-12 11:24:06 +0100

    add weighted quick union implementation

--- src/main/scala/dynCon/QuickUnion.scala
+++ src/main/scala/dynCon/QuickUnion.scala
@@ -14,4 +14,20 @@ class QuickUnion(elements: List[Int]) extends UF {
   @tailrec
   final def rootFor(p: Int): Int = if (elements(p) == p) p else rootFor(elements(p))
 
+  override def toString = elements.mkString(";")
+
+}
+
+
+class WeightedQuickUnion(elements: List[Int], weights: List[Int]) extends QuickUnion(elements) {
+  def this(n: Int) = {
+    this(0 to n - 1 toList, List.fill(n)(1))
+  }
+
+  override def union(p: Int, q: Int): UF =
+    if (weights(p) > weights(q))
+      new WeightedQuickUnion(elements.updated(rootFor(q), rootFor(p)), weights.updated(q, weights(q) + 1))
+    else
+      new WeightedQuickUnion(elements.updated(rootFor(p), rootFor(q)), weights.updated(p, weights(p) + 1))
+
 }
--- src/test/scala/dynCon/UFTest.scala
+++ src/test/scala/dynCon/UFTest.scala
@@ -6,11 +6,16 @@ import org.scalatest.FlatSpec
 class UFTest extends FlatSpec with UFBehaviors {
 
   def quickFind = new QuickFind(10)
-
   def quickUnion = new QuickUnion(10)
+  def weightedQuickUnion = new WeightedQuickUnion(10)
 
   "Quick find " should behave like _10SizedUF(quickFind)
   "Quick union " should behave like _10SizedUF(quickUnion)
+  "Weighted quick union " should behave like _10SizedUF(weightedQuickUnion)
+    it should "be weighted " in {
+      val string=weightedQuickUnion.union(3, 0).union(3, 8).union(8, 2).union(2, 5).toString
+      assert(string==="0;1;0;0;4;0;6;7;0;9")
+    }
 }
 
 trait UFBehaviors {
