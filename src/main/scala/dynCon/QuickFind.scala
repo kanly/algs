5204a76de1d4ccab795932d22014e94677de3e35
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-04-05 18:33:20 +0200

    No need of finding root here

--- src/main/scala/dynCon/QuickFind.scala
+++ src/main/scala/dynCon/QuickFind.scala
@@ -7,12 +7,9 @@ class QuickFind(elements: List[Int]) extends UF {
     this(0 to n - 1 toList)
   }
 
-  def union(p: Int, q: Int): UF = new QuickFind(elements.map(x => if (x == p) rootFor(q) else x))
+  def union(p: Int, q: Int): UF = new QuickFind(elements.map(x => if (x == p) q else x))
 
   def connected(p: Int, q: Int): Boolean = elements(p) == elements(q)
-
-  @tailrec
-  final def rootFor(p: Int): Int = if (elements(p) == p) p else rootFor(elements(p))
 
   override def toString = elements.mkString(";")
 }
