251e2db7f1b34b735c1a9a6e628acd0f89af6e00
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-04-08 11:44:02 +0200

    wrong pattern matching usage!

--- src/test/scala/sort/SortTest.scala
+++ src/test/scala/sort/SortTest.scala
@@ -3,8 +3,8 @@ package sort
 import org.scalatest.FlatSpec
 
 
-class SortTest extends FlatSpec with SortBehaviors  {
-  def insertion=new Insertion[Int]
+class SortTest extends FlatSpec with SortBehaviors {
+  def insertion = new Insertion[Int]
 
   "Insertion " should behave like _plainSorter(insertion)
 }
@@ -12,14 +12,14 @@ class SortTest extends FlatSpec with SortBehaviors  {
 trait SortBehaviors {
   this: FlatSpec =>
   def _plainSorter(sorter: Sort[Int]) {
-    def sorterFunction(first:Int,second:Int) = (first-second) match {
-      case _ > 0 => Bigger
-      case _ < 0 => Smaller
-      case _ => Equal
-    }
+    def sorterFunction(first: Int, second: Int) =
+      if (first > second) Bigger
+      else if (first < second) Smaller
+      else Equal
+
     it should "sort correctly (2,1,4,5,6,1,6,3,1)" in {
-      val sorted=sorter.sort(List(2,1,4,5,6,1,6,3,1))
-      assert(sorted==List(1,1,1,2,3,4,5,6,6))
+      val sorted = sorter.sort(List(2, 1, 4, 5, 6, 1, 6, 3, 1))
+      assert(sorted == List(1, 1, 1, 2, 3, 4, 5, 6, 6))
     }
   }
 }
