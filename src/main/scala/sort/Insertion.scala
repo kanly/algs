7d94b17634d904098da4e5e1bde4366e1d4a0865
Paolo Mazzoncini <p.mazzoncini@gmail.com>
2013-04-09 12:24:30 +0200

    Insertion Sort

--- src/main/scala/sort/Insertion.scala
+++ src/main/scala/sort/Insertion.scala
@@ -1,8 +1,12 @@
 package sort
 
 
-class Insertion[A](comparator: (A, A) => CompareResult)(elements:List[A]) extends Sorter[A](comparator)(elements){
-  override def sort(): Sorter[A] = ???
+class Insertion[A](comparator: (A, A) => CompareResult)(elements: List[A]) extends Sorter[A](comparator)(elements) {
+  override def sort(): Sorter[A] = newInstance(elements.foldLeft(List[A]())(insert))
+
+  def insert(list: List[A], value:A) = list.span(firstIsLess(_,value)) match {
+    case (lower,upper) => lower :::  value :: upper
+  }
 
   override def newInstance(elems: List[A]): Sorter[A] = new Insertion[A](comparator)(elems)
 }
